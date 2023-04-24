83e5ca6a2b456805b7cf3a20f6d4a038e1dba627
Devin Jones <devin.jones21083@gmail.com>
2023-04-24 14:29:11 -0400

    Edited index.js

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Add your code here
+const submitData = function (userName, userEmail) {
+    
+    const formData = {
+        name: userName,
+        email: userEmail,
+    };
+    const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+    };
+    debugger;
+    
+    return fetch ("http://localhost:3000/users", configurationObject)
+    .then(function (resp) {
+        return resp.json();
+    })
+    .then(function (data) {
+        //const currentUser = data;
+        //console.log(data);
+        const docBody = document.querySelector("body");
+        const idNumber = data.id;
+        //console.log(idNumber);
+        docBody.append(idNumber);
+
+    })
+    .catch(function (object) {
+        alert("Danger, Danger");
+        const errMessage = object.message;
+        console.log(errMessage);
+        const docBody = document.querySelector("body");
+        docBody.append(errMessage);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', submitData("Devin", "fakeEmail@gmail.com"));
